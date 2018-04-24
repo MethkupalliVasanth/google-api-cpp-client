0433109786983987136b43a8d0674a8164df884d
vasmethk <mvasanthiiit@gmail.com>
2018-04-23 23:29:29 -0400

    Update keycloak_sample_main.cc

--- src/samples/keycloak_sample_main.cc
+++ src/samples/keycloak_sample_main.cc
@@ -105,23 +105,13 @@ if(!status.ok()){
 
 
 void KeyCloakSample::Run() {
-  // Connect the credential passed to ListBuckets() with the AuthFlow
-  // constructed in Startup().
+
   credential_.set_flow(flow_.get());
-  // Construct the request.
-  google_storage_api::BucketsResource_ListMethod request(
-      storage_.get(), &credential_, flow_->project_id());
+  // Construct the request for running the Airavata API
+  
   Json::Value value;
-  google_storage_api::Buckets buckets(&value);
-  // Execute the request.
-  auto status = request.ExecuteAndParseResponse(&buckets);
-  if (!status.ok()) {
-    std::cout << "Could not list buckets: " << status.error_message()
-              << std::endl;
-  }
-  for (auto bucket: buckets.get_items()) {
-    std::cout << bucket.get_self_link() << std::endl;
-  }
+  // need to wrie implementation details of running the Airavata API using the credential store.
+  //this can be taken from the client C++ SDKs.
 }
 
 
@@ -139,7 +129,7 @@ if(curl){
 
 	curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8080/auth/realms/master/protocol/openid-connect/token");
 	curl_easy_setopt(curl, CURLOPT_POST, 1);
-	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "client_id=client_account&client_secret=b770ebe8-e75f-4acf-a3ef-e45c093d1743&username=admin&password=admin&grant_type=password");
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "client_id=client_account&client_secret=secret&username=user&password=password&grant_type=password");
 res=curl_easy_perform(curl);
 curl_easy_cleanup(curl);
 return res
@@ -155,12 +145,14 @@ int main(int argc, char* argv[]) {
 
   KeycloakSample sample;
   googleapis::util::Status status = sample.Startup(argc, argv);
+  // essentially creating the workflow and credential store, taking the status and checking
   if (!status.ok()) {
     std::cerr << "Could not initialize application." << std::endl;
     std::cerr << status.error_message() << std::endl;
     return -1;
   }
-
+	
+  sample.Curl();
   sample.Run();
   std::cout << "Done!" << std::endl;
 
@@ -170,54 +162,3 @@ int main(int argc, char* argv[]) {
 
 
 
-
-int main(){
-
-CURL *curl;
-CURLcode res;
-
-curl=curl_easy_init();
-
-if(curl){
-
-	curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8080/auth/realms/master/protocol/openid-connect/token");
-	curl_easy_setopt(curl, CURLOPT_POST, 1);
-	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "client_id=client_account&client_secret=b770ebe8-e75f-4acf-a3ef-e45c093d1743&username=admin&password=admin&grant_type=password");
-res=curl_easy_perform(curl);
-curl_easy_cleanup(curl);
-
-
-
-
-string store_path;
-googleapis::util::Status status= FileCredentialStoreFactory::GetSystemHomeDirectoryStorePath(&store_path);
-
-
-flow_.reset(OAuth2AuthorizationFlow::MakeFlowFromClientSecretsJson(res, transport, &status));
-CHECK(status.ok())<< status.error_message();
-
-
-
-
-
-if(status.ok()){
-	FileCredentialStoreFactory store_factory(store_path);
-	flow_->ResetCredentialSotre(store_factory.NewCredentialStore("KeyCloakSample", &status));
-}
-if(!status.ok()){
-	LOG(ERROR)
-		<< "FAILED CREATING CREDENTIAL STORE:"<< status.error_message();
-}
-
-//saving a credential to the credential store
-
-status=credential_store->Store(user_id, credential);
-if(!status.ok()){
-	LOG(ERROR) << "Failed Storing credentials:"<< status.error_message();
-}
-
-
-
-}
-return 0;
-}
